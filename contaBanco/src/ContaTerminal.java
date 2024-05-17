96fb0ea44a9e14b0b2855048eb73bfa4d3a651d4
Beatriz Galliera <beagalliera@gmail.com>
2024-05-17 09:57:26 -0300

    adiciona gerenciamento de conta e gitignore

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args){
+        //Conhecer e importar a classe Scanner
+
+        //Exibir as mensagens para o nosso usuário
+
+        //Obter pela scanner os valores digitados no terminal
+
+        //Exibir a mesagem conta criada
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Informe o seu nome completo: ");
+        String nome = entrada.nextLine();
+
+        System.out.println("Digite o número da agência: ");
+        String agencia = entrada.next();
+
+        int numero = 1021;
+        double saldo = 237.58d;
+
+        System.out.println("Olá " + nome + 
+        ", obrigado por criar uma conta em nosso banco, sua agência é " + 
+        agencia +", conta " + numero + " e seu saldo " + saldo + 
+        " já está disponível para saque");
+        
+        entrada.close();
+
     }
 }
